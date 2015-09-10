bd587fa40b3fcdcfdf4a8a7d9b86f94f8bc75601
darshilgit <darshil.parikh@gmail.com>
2015-09-10 15:50:49 +0530

    Update about.php

--- about.php
+++ about.php
@@ -100,7 +100,7 @@
             </div>
         </div>
 
-        <!-- Team Members Row -->
+        <!-- Team Members-->
         <div class="row">
             <div class="col-lg-12">
                 <h2 class="page-header">Our Team</h2>
@@ -177,3 +177,5 @@
 </body>
 
 </html>
+
+
